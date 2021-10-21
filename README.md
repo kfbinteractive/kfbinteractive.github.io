47d66e44ba9b9e63d807d6d7923ba7b3aa77beb2
iolanisimon <86267191+iolanisimon@users.noreply.github.com>
2021-10-20 21:24:03 -1000

    Reupload

---
+++ README.md
@@ -0,0 +1,1 @@
+<a href="kfbinteractive.github.io">kfbinteractive.github.io</a>
---
+++ all.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("browsersensor").style.display = "block";
+};
+
+//or
+
+window.addEventListener("load", function(){
+    document.getElementById("loader").style.display = "none";
+});
