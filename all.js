cfec87230c498731405f83d1323a09a94d9b5801
iolanisimon <86267191+iolanisimon@users.noreply.github.com>
2021-10-21 19:52:42 -1000

    Revamped

--- all.js
+++ all.js
@@ -1,16 +1,23 @@
+const cardbg = document.querySelector('.cardbg');
+
 window.onload = function(){
+        document.getElementById("browsersensor").style.display = "block";       
+        document.getElementById("loader").style.opacity = "0";     
+    document.getElementById("browsersensor").style.display = "block";
     setTimeout(function(){
-        document.getElementById("loader").style.animationPlayState = "running";
-    },1000); 
+        document.getElementById("loader").style.animationPlayState = "running";    
+        document.getElementById("loader").style.opacity = "0.3";       
+    },100); 
+     
     
+    setTimeout(function(){
+        document.getElementById("loader").style.opacity = "0";
+        document.getElementById("browsersensor").style.opacity = "1";
+    },2300); 
     
     setTimeout(function(){
         document.getElementById("loader").style.display = "none";
-    },10000); 
+        
+    },2400); 
 };
 
-//or
-
-// window.addEventListener("load", function(){
-//    document.getElementById("loader").style.display = "none";
-// });
