280881b70d3403dd3d8c1f6f71b04662ba294214
iolanisimon <86267191+iolanisimon@users.noreply.github.com>
2021-11-06 13:16:59 -1000

    v1.2.0

---
+++ birds/birds.js
@@ -0,0 +1,10 @@
+function cardScale() {
+    var element = document.getElementById("cardchange1");
+    element.classList.toggle("cardchange");
+  }
+
+  function cardScale1() {
+    var element1 = document.getElementById("cardchange2");
+    
+    element1.classList.toggle("cardchange");
+  }
